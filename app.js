2fff94be6509aa66a2e262f76a0ed8f5d2e583ae
lkic1625 <lkic1625@gmail.com>
2020-10-18 13:59:55 +0900

    modify app.js

--- app.js
+++ app.js
@@ -1,41 +1,81 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const morgan = require('morgan');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const fs = require('fs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config();
+const logger = require('./logger');
+const { sequelize } = require('./models');
 
-var app = express();
+passportConfig();
+app.set('port', process.env.PORT || 14024);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true, 
+});
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+if (process.env.NODE_ENV === 'production'){
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(hpp());
+} else{
+  app.use(morgan('dev'));
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const sessionOptions = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}
+if (process.env.NODE_ENV === 'production'){
+  sessionOptions.proxy = true;
+  sessionOptions.cookie.secure = true;
+}
+app.use(session(sessionOptions));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//TODO: router
+
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  logger.error(error.message);
+  next(error);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+app.listen(app.get('port'), () => {
+  logger.info(app.get('port'), '번 포트에서 대기중');
+});
